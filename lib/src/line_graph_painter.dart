7bbb7fc1b599ba3dc7e83340801277fc6878f33c
Mincheol Shin <55150540+mincheol-shin@users.noreply.github.com>
2021-05-30 12:46:52 +0900

    Add comment

--- lib/src/line_graph_painter.dart
+++ lib/src/line_graph_painter.dart
@@ -32,16 +32,19 @@ class LineGraphPainter extends CustomPainter {
 
     /// 첫 시작 지점
     /// y축 출력시 + 50.0
-    double startPoint = 50.0;
+    double startPoint = 100.0;
 
-    double xAxisCenter = 40.0;
+    /// x축 width
+    double xAxisWidth = 40.0;
 
+
+    /// 차트 총 높이에서 x축 높이 제거
     double chartHeight = height - 40.0;
 
-    /// Offset(xAxis, yAxis)
+    /// xAxis : 첫 시작 지점 + x축 width * x축의 index
     for (int i = 0; i < data.length - 1; i++) {
-      points.add(Offset(startPoint + (xAxisCenter * i), chartHeight - (data[i] - (minY - (yAxisSpacing / 2))) * (yBoxHeight / yAxisSpacing) - 9.0));
-      points.add(Offset(startPoint + xAxisCenter * (i + 1), chartHeight - (data[i + 1] - (minY - (yAxisSpacing / 2))) * (yBoxHeight / yAxisSpacing) - 9.0));
+      points.add(Offset(startPoint + (xAxisWidth * i), chartHeight - (data[i] - (minY - (yAxisSpacing / 2))) * (yBoxHeight / yAxisSpacing) - 9.0));
+      points.add(Offset(startPoint + xAxisWidth * (i + 1), chartHeight - (data[i + 1] - (minY - (yAxisSpacing / 2))) * (yBoxHeight / yAxisSpacing) - 9.0));
     }
 
     canvas.drawPoints(PointMode.lines, points, paint);
